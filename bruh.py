cb48859d210286d4fd9e31e5a2fd113f62903fc4
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2022-02-15 19:33:36 +0000

    Update bruh.py

--- bruh.py
+++ bruh.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
 import sys
-
 
 TAB_CHAR = '\t'
 
@@ -8,3 +6,10 @@ for line in sys.stdin:
     for token in line.strip().split(" "):
         if token: 
             print(token + TAB_CHAR + '1')
+            
+for line in sys.stdin:
+    words = line.strip().split(" ")
+    if len(words) % 2 == 0:
+        println('even' + TAB_CHAR + '1')
+    else:
+        println('odd' + TAB_CHAR + '1')
