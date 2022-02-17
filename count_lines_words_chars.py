5626f8c2d24ba6ca656e1536c79d6e44ff1e99a0
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2022-02-17 19:46:17 +0000

    Update count_lines_words_chars.py

--- count_lines_words_chars.py
+++ count_lines_words_chars.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python
 import sys
 
 TAB_CHAR = '\t'
 
 for line in sys.stdin:
-    print("line" + TAB_CHAR + '1')
+    print('Number of lines:' + TAB_CHAR + '1')
     for token in line.strip().split(" "):
         if token: 
-            print("word" + TAB_CHAR + '1')
+            print('Number of words:' + TAB_CHAR + '1')
             for token3 in token:
                 if token3:
-                    print('characters' + TAB_CHAR + '1')
+                    print('Number of characters:' + TAB_CHAR + '1')
