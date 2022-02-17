3da109b790a00ed8d09c4c8c18eff3cd3ede536e
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2022-02-17 19:47:27 +0000

    Update count_reduce.py

--- count_reduce.py
+++ count_reduce.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python
 import sys
 
-
 TAB_CHAR = '\t'
-
 lastKey = None
 sum = 0
 
 for line in sys.stdin:
     key, value = line.strip().split(TAB_CHAR)
-
     if lastKey and lastKey != key:
         print(lastKey + TAB_CHAR + str(sum))
         lastKey = key
