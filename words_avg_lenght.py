4ee73910c07799405ca6f035a5b0ded14a1d0069
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2022-02-17 19:48:26 +0000

    Update words_avg_lenght.py

--- words_avg_lenght.py
+++ words_avg_lenght.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python
 import sys
 
 TAB_CHAR = '\t'
 
 for line in sys.stdin:
-    for token in line.strip().split(" "):
+    for token in line.strip().split(' '):
          if token:
-            print('word' + TAB_CHAR + str(len(token)))
-
-                
+            print('Number of words:' + TAB_CHAR + str(len(token)))
