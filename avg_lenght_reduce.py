06e4416d96044e24b402419fb28255c55200fe83
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2022-02-17 19:39:26 +0000

    Update avg_lenght_reduce.py

--- avg_lenght_reduce.py
+++ avg_lenght_reduce.py
@@ -1,25 +1,20 @@
-#!/usr/bin/python
 import sys
 
-
 TAB_CHAR = '\t'
-
 lastKey = None
 sum = 0
 count = 0
+
 for line in sys.stdin:
     key, value = line.strip().split(TAB_CHAR)
-
     if lastKey and lastKey != key:
         print(lastKey + TAB_CHAR + str(sum))
         lastKey = key
         sum = int(value)
         count += 1
-        
     else:
         lastKey = key
         sum += int(value)
         count += 1
-
 if lastKey:
-    print('word' + TAB_CHAR + str(sum/count))
+    print('number of words:' + TAB_CHAR + str(sum/count))
