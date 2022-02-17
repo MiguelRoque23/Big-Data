a4a074e6a38a1eb91ed553c54080aad9a5055910
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2022-02-17 20:23:24 +0000

    Delete words_avg_lenght.py

--- words_avg_lenght.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import sys
-
-TAB_CHAR = '\t'
-
-for line in sys.stdin:
-    for token in line.strip().split(' '):
-         if token:
-            print('Number of words:' + TAB_CHAR + str(len(token)))
